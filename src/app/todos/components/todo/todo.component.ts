072979d945712e725399f126259c73d33189fd4f
Anastasiia Sabodash <anastasiia.sabodash@itechcraft.com>
2021-08-12 22:17:48 +0200

    added formControl to todo.component

--- src/app/todos/components/todo/todo.component.ts
+++ src/app/todos/components/todo/todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 import { TodoModel } from '../../models/todo.model';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -12,23 +13,22 @@ export class TodoComponent implements OnInit {
   @Input('todo') todo: TodoModel;
   @Input('isEditing') isEditing: boolean;
   // TODO: Remove excessive typing. We can use simple new EventEmitter<T>(); for it
-  // TODO: Please, rename to editingId, editIdChange.
   // We can interpret this event emitter like a simple html element event, like click, change and so on.
-  @Output('setEditingId') setEditingIdEvent: EventEmitter<string | null> =
+  @Output('editingId') editingId: EventEmitter<string | null> =
     new EventEmitter<string | null>();
 
   // TODO: Replace to FormControl
-  public editingText: string = '';
+  public editingText = new FormControl();
 
   // TODO: Please, add readonly modifier
   constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-    this.editingText = this.todo.text;
+    this.editingText.setValue(this.todo.text);
   }
 
   public setTodoInEditMode(): void {
-    this.setEditingIdEvent.emit(this.todo.id);
+    this.editingId.emit(this.todo.id);
   }
 
   // TODO: Please, move removeTodo, toggleTodo and changeTodo handling to parent component
@@ -43,15 +43,9 @@ export class TodoComponent implements OnInit {
     this.todoService.changeStatus(this.todo.id);
   }
 
-  public changeText(event: Event): void {
-    // TODO: Remove after refactor to form control
-    const value = (event.target as HTMLInputElement).value;
-    this.editingText = value;
-  }
-
   public changeTodo(): void {
     // TODO: Please, revisit this approach, and move changeTodo into parent and manage finish editing at that level
-    this.todoService.changeText(this.todo.id, this.editingText);
-    this.setEditingIdEvent.emit(null);
+    this.todoService.changeText(this.todo.id, this.editingText.value);
+    this.editingId.emit(null);
   }
 }
